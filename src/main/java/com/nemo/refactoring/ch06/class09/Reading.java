4b5446e9cbb1c715f98c1c80abfea810d25a51ad
yonghwankim-dev <dragonbead95@naver.com>
2024-08-09 13:19:44 +0900

    refactor: calculateBaseCharge private

--- src/main/java/com/nemo/refactoring/ch06/class09/Reading.java
+++ src/main/java/com/nemo/refactoring/ch06/class09/Reading.java
@@ -15,7 +15,11 @@ public class Reading {
 		this.time = time;
 	}
 
-	public double calculateBaseCharge() {
+	public double calculateTaxableCharge(){
+		return Math.max(0, calculateBaseCharge() - taxThreshold());
+	}
+
+	private double calculateBaseCharge() {
 		return baseRate() * quantity;
 	}
 
@@ -25,10 +29,6 @@ public class Reading {
 		baseMap.put(2024,  new double[]{0.1, 0.1, 0.12, 0.12, 0.15, 0.15, 0.18, 0.18, 0.2, 0.2, 0.22, 0.22});
 
 		return baseMap.get(time.getYear())[time.getMonthValue() - 1];
-	}
-
-	public double calculateTaxableCharge(){
-		return Math.max(0, calculateBaseCharge() - taxThreshold());
 	}
 
 	public double taxThreshold() {
--- src/test/java/com/nemo/refactoring/ch06/class09/ReadingTest.java
+++ src/test/java/com/nemo/refactoring/ch06/class09/ReadingTest.java
@@ -1,7 +1,5 @@
 package com.nemo.refactoring.ch06.class09;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.time.LocalDate;
 
 import org.assertj.core.api.Assertions;
@@ -10,18 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ReadingTest {
-	@DisplayName("calculate baseCharge")
-	@Test
-	void calculateBaseCharge(){
-	    // given
-		Reading reading = new Reading("kim", 10, LocalDate.of(2024, 8, 9));
-	    // when
-		// 0.18 * 10 = 1.8
-		double baseCharge = reading.calculateBaseCharge();
-	    // then
-		Assertions.assertThat(baseCharge).isCloseTo(1.8, Offset.offset(0.1));
-	}
-
 	@DisplayName("calculate TaxableCharge for Reading")
 	@Test
 	void calculateTaxableCharge(){
