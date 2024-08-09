8a1c3299cebb4c69e233dad62f87bd4e50611fcf
yonghwankim-dev <dragonbead95@naver.com>
2024-08-09 13:51:53 +0900

    feat: taxableCharge 메서드 구현

--- src/main/java/com/nemo/refactoring/ch06/class10/Reading.java
+++ src/main/java/com/nemo/refactoring/ch06/class10/Reading.java
@@ -18,7 +18,7 @@ public class Reading {
 
 	public static EnrichReading enrichReading(String customer, int quantity, int month, int year){
 		double baseCharge = baseCharge(year, month, quantity);
-		double taxableCharge = taxableCharge();
+		double taxableCharge = taxableCharge(baseCharge, taxThreshold(year));
 		return new EnrichReading(baseCharge, taxableCharge);
 	}
 
@@ -34,7 +34,15 @@ public class Reading {
 		return baseMap.get(year)[month - 1];
 	}
 
-	private static double taxableCharge() {
-		return 1.6;
+	private static double taxableCharge(double baseCharge, double taxThreshold){
+		return Math.max(0, baseCharge - taxThreshold);
+	}
+
+	private static double taxThreshold(int year) {
+		Map<Integer, Double> taxMap = new HashMap<>();
+		taxMap.put(2023, 0.2);
+		taxMap.put(2024, 0.2);
+
+		return taxMap.getOrDefault(year, 0.0);
 	}
 }
