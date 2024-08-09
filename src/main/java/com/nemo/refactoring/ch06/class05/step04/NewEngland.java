89abddedc97adcf897b3c871a09d8911a404b20a
yonghwankim-dev <dragonbead95@naver.com>
2024-08-09 11:26:48 +0900

    feat: 기존 메서드 제거

--- src/main/java/com/nemo/refactoring/ch06/class05/step04/NewEngland.java
+++ src/main/java/com/nemo/refactoring/ch06/class05/step04/NewEngland.java
@@ -6,9 +6,9 @@ public class NewEngland {
 
 	private List<String> states = List.of("MA", "CT", "ME", "VT", "NH", "RI");
 
-	public boolean isNewEngland(Customer customer) {
-		return isNewEngland(customer.getAddress().getState());
-	}
+	// public boolean isNewEngland(Customer customer) {
+	// 	return isNewEngland(customer.getAddress().getState());
+	// }
 
 	public boolean isNewEngland(String state) {
 		return states.contains(state);
