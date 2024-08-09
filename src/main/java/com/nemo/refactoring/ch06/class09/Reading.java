628742bd9032d5d6e3d87292ed51408a90ebca1d
yonghwankim-dev <dragonbead95@naver.com>
2024-08-09 13:11:46 +0900

    style: 코드 정리

--- src/main/java/com/nemo/refactoring/ch06/class09/Reading.java
+++ src/main/java/com/nemo/refactoring/ch06/class09/Reading.java
@@ -28,16 +28,15 @@ public class Reading {
 	}
 
 	public double calculateBaseCharge() {
-		return baseRate(time.getMonthValue(), time.getYear()) * quantity;
+		return baseRate() * quantity;
 	}
 
-	public double baseRate(int month, int year) {
-		Map<Integer, double[]> baseMap = new HashMap(){{
-			put(2023,  new double[]{0.1, 0.1, 0.12, 0.12, 0.15, 0.15, 0.18, 0.18, 0.2, 0.2, 0.22, 0.22});
-			put(2024,  new double[]{0.1, 0.1, 0.12, 0.12, 0.15, 0.15, 0.18, 0.18, 0.2, 0.2, 0.22, 0.22});
-		}};
+	public double baseRate() {
+		Map<Integer, double[]> baseMap = new HashMap<>();
+		baseMap.put(2023,  new double[]{0.1, 0.1, 0.12, 0.12, 0.15, 0.15, 0.18, 0.18, 0.2, 0.2, 0.22, 0.22});
+		baseMap.put(2024,  new double[]{0.1, 0.1, 0.12, 0.12, 0.15, 0.15, 0.18, 0.18, 0.2, 0.2, 0.22, 0.22});
 
-		return baseMap.get(year)[month - 1];
+		return baseMap.get(time.getYear())[time.getMonthValue() - 1];
 	}
 
 	public static double taxThreshold(int year) {
