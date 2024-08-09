cc7016ff6c7674a91b904d8a3e86c146cc1c3803
yonghwankim-dev <dragonbead95@naver.com>
2024-08-09 12:29:34 +0900

    feat: NumberRange 클래스 생성
    온도의 최솟값과 최댓값을 저장하는 객체

---
+++ src/main/java/com/nemo/refactoring/ch06/class08/NumberRange.java
@@ -0,0 +1,19 @@
+package com.nemo.refactoring.ch06.class08;
+
+public class NumberRange {
+	private int min;
+	private int max;
+
+	public NumberRange(int min, int max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	public int getMin() {
+		return min;
+	}
+
+	public int getMax() {
+		return max;
+	}
+}
