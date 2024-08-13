0a92ad9ed2a14aa5f0fa0bb9face1c665399c111
yonghwankim-dev <dragonbead95@naver.com>
2024-08-13 12:26:48 +0900

    test: OrderReader countOrders 메서드의 테스트 추가

--- src/main/java/com/nemo/refactoring/ch06/class11/step02/OrderReader.java
+++ src/main/java/com/nemo/refactoring/ch06/class11/step02/OrderReader.java
@@ -12,24 +12,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class OrderReader {
 	public static void main(String[] args) {
 		try {
-			System.out.println(run(args));
+			OrderReader orderReader = new OrderReader();
+			System.out.println(orderReader.run(args));
 		} catch (IOException e) {
 			System.err.println(e);
 			System.exit(1);
 		}
 	}
 
-	private static long run(String[] args) throws IOException {
+	public long run(String[] args) throws IOException {
 		if (args.length == 0) {
 			throw new RuntimeException("input the file name.");
 		}
 		String fileName = args[args.length - 1];
+		return countOrders(args, fileName);
+	}
+
+	private long countOrders(String[] args, String fileName) throws IOException {
 		File input = new ClassPathResource(fileName).getFile();
 		ObjectMapper mapper = new ObjectMapper();
 		Order[] orders = mapper.readValue(input, Order[].class);
 		if (Stream.of(args).anyMatch(arg-> "-r".equals(arg))){
 			return Stream.of(orders)
-				.filter(o->"ready".equals(o.getStatus()))
+				.filter(o -> "ready".equals(o.getStatus()))
 				.count();
 		}else{
 			return orders.length;
---
+++ src/test/java/com/nemo/refactoring/ch06/class11/step02/OrderReaderTest.java
@@ -0,0 +1,24 @@
+package com.nemo.refactoring.ch06.class11.step02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderReaderTest {
+
+	@DisplayName("json 파일을 읽어서 ready 상태인 주문의 개수를 센다")
+	@Test
+	void countOrders() throws IOException {
+	    // given
+		OrderReader orderReader = new OrderReader();
+		// when
+		long count = orderReader.run(new String[] {"-r", "orders.json"});
+		// then
+		Assertions.assertThat(count).isEqualTo(2L);
+	}
+
+}
