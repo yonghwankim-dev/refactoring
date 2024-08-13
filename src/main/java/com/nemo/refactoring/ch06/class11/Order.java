185c5638a886cb6137820551498bd75fc439daf6
yonghwankim-dev <dragonbead95@naver.com>
2024-08-13 11:49:23 +0900

    refactor: quantity 제거하여 PriceData에 이동

--- src/main/java/com/nemo/refactoring/ch06/class11/Order.java
+++ src/main/java/com/nemo/refactoring/ch06/class11/Order.java
@@ -4,26 +4,13 @@ public class Order {
 	public int priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
 		int basePrice = product.getBasePrice() * quantity;
 		int discount = (int)(Math.max(0, quantity - product.getDiscountThreshold()) * product.getBasePrice() * product.getDiscountRate());
-		PriceData priceData = new PriceData(basePrice);
-		return applyShipping(priceData, shippingMethod, quantity, discount);
+		PriceData priceData = new PriceData(basePrice, quantity);
+		return applyShipping(priceData, shippingMethod, discount);
 	}
 
-	private int applyShipping(PriceData priceData, ShippingMethod shippingMethod, int quantity, int discount) {
+	private int applyShipping(PriceData priceData, ShippingMethod shippingMethod, int discount) {
 		int shippingPerCase = (priceData.getBasePrice() > shippingMethod.getDiscountThreshold()) ? shippingMethod.getDiscountFee() : shippingMethod.getFeePerCase();
-		int shippingCost = quantity * shippingPerCase;
+		int shippingCost = priceData.getQuantity() * shippingPerCase;
 		return priceData.getBasePrice() - discount + shippingCost;
-	}
-
-	public static void main(String[] args) {
-		Order order = new Order();
-		Product car = new Product(10_000_000, 0.1, 3);
-		int quantity = 5;
-
-		int discountThreshold = 500_000;
-		int discountFee = 500_000;
-		int feePerCase = 100_000;
-		ShippingMethod shippingMethod = new ShippingMethod(discountThreshold, discountFee, feePerCase);
-		int price = order.priceOrder(car, quantity, shippingMethod);
-		System.out.println(price); // 50_500_000
 	}
 }
--- src/main/java/com/nemo/refactoring/ch06/class11/PriceData.java
+++ src/main/java/com/nemo/refactoring/ch06/class11/PriceData.java
@@ -3,12 +3,18 @@ package com.nemo.refactoring.ch06.class11;
 
 public class PriceData {
 	private final int basePrice;
+	private final int quantity;
 
-	public PriceData(int basePrice) {
+	public PriceData(int basePrice, int quantity) {
 		this.basePrice = basePrice;
+		this.quantity = quantity;
 	}
 
 	public int getBasePrice() {
 		return basePrice;
+	}
+
+	public int getQuantity() {
+		return quantity;
 	}
 }
