3895506f924f14bae0a7fe19547c989728dff75f
yonghwankim-dev <dragonbead95@naver.com>
2024-08-16 12:32:48 +0900

    feat: overdraftCharge 메서드를 복사하여 AccountType에 이동

--- src/main/java/com/nemo/refactoring/ch08/class01/step02/Account.java
+++ src/main/java/com/nemo/refactoring/ch08/class01/step02/Account.java
@@ -3,11 +3,24 @@ package com.nemo.refactoring.ch08.class01.step02;
 public class Account {
 
 	enum AccountType {
-			PREMIUM, NORMAL;
+		PREMIUM, NORMAL;
 
-			public boolean isPremium() {
-				return this == PREMIUM;
+		public boolean isPremium() {
+			return this == PREMIUM;
+		}
+
+		// 초과 인출 이자 계산
+		public double overdraftCharge(int daysOverdrawn) {
+			if (this.isPremium()) {
+				double baseCharge = 10;
+				if (daysOverdrawn <= 7) {
+					return baseCharge;
+				}
+				return baseCharge + (daysOverdrawn - 7) * 0.85;
+			}else{
+				return daysOverdrawn * 1.75;
 			}
+		}
 	}
 
 	private int daysOverdrawn; // 마이너스 잔액일 된 일수
