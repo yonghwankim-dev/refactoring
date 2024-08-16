558c633037552524bc518676f03eb61b24b5730d
yonghwankim-dev <dragonbead95@naver.com>
2024-08-16 11:06:19 +0900

    feat: Person, Department 클래스 추가

---
+++ src/main/java/com/nemo/refactoring/ch07/class07/step01/Department.java
@@ -0,0 +1,19 @@
+package com.nemo.refactoring.ch07.class07.step01;
+
+public class Department {
+	private String chargeCode;
+	private String manager;
+
+	public Department(String chargeCode, String manager) {
+		this.chargeCode = chargeCode;
+		this.manager = manager;
+	}
+
+	public String getChargeCode() {
+		return chargeCode;
+	}
+
+	public String getManager() {
+		return manager;
+	}
+}
---
+++ src/main/java/com/nemo/refactoring/ch07/class07/step01/Person.java
@@ -0,0 +1,19 @@
+package com.nemo.refactoring.ch07.class07.step01;
+
+public class Person {
+	private String name;
+	private Department department;
+
+	public Person(String name, String chargeCode, String manager) {
+		this.name = name;
+		this.department = new Department(chargeCode, manager);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+}
