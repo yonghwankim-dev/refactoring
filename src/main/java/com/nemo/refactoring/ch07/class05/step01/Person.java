944ed0a611097c9f20c7ff1181c3395cd6c88467
yonghwankim-dev <dragonbead95@naver.com>
2024-08-16 10:21:00 +0900

    feat: Perrson 클래스 구현

---
+++ src/main/java/com/nemo/refactoring/ch07/class05/step01/Person.java
@@ -0,0 +1,37 @@
+package com.nemo.refactoring.ch07.class05.step01;
+
+public class Person {
+	private String name;
+	private String officeAreaCode;
+	private String officeNumber;
+
+	public Person(String name, String officeAreaCode, String officeNumber) {
+		this.name = name;
+		this.officeAreaCode = officeAreaCode;
+		this.officeNumber = officeNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getOfficeAreaCode() {
+		return officeAreaCode;
+	}
+
+	public void setOfficeAreaCode(String officeAreaCode) {
+		this.officeAreaCode = officeAreaCode;
+	}
+
+	public String getOfficeNumber() {
+		return officeNumber;
+	}
+
+	public void setOfficeNumber(String officeNumber) {
+		this.officeNumber = officeNumber;
+	}
+}
