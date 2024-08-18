530f80fb76eebfd96abdd89276ca84dc933610b3
yonghwankim-dev <dragonbead95@naver.com>
2024-08-18 14:00:30 +0900

    refactor: getPrimary로 메서드 추출

--- src/main/java/com/nemo/refactoring/ch09/class01/Haggis.java
+++ src/main/java/com/nemo/refactoring/ch09/class01/Haggis.java
@@ -3,9 +3,13 @@ package com.nemo.refactoring.ch09.class01;
 public class Haggis {
 
 	public double distanceTravelled(Scenario scenario, int time){
-		double primaryTime = Math.min(time, scenario.getDelay());
 		double secondaryTime = time - scenario.getDelay();
-		return (0.5 * scenario.primaryAcceleration() * primaryTime * primaryTime) + getSecondary(scenario, secondaryTime);
+		return getPrimary(scenario, time) + getSecondary(scenario, secondaryTime);
+	}
+
+	private double getPrimary(Scenario scenario, int time) {
+		double primaryTime = Math.min(time, scenario.getDelay());
+		return 0.5 * scenario.primaryAcceleration() * primaryTime * primaryTime;
 	}
 
 	private double getSecondary(Scenario scenario, double secondaryTime) {
