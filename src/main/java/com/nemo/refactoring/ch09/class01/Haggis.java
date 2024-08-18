001aadab674c9fa95757bb64763a7219c17c69a4
yonghwankim-dev <dragonbead95@naver.com>
2024-08-18 14:02:59 +0900

    refactor: 변수 인라인

--- src/main/java/com/nemo/refactoring/ch09/class01/Haggis.java
+++ src/main/java/com/nemo/refactoring/ch09/class01/Haggis.java
@@ -4,11 +4,11 @@ public class Haggis {
 
 	public double distanceTravelled(Scenario scenario, int time){
 		double secondaryTime = time - scenario.getDelay();
-		return getPrimary(scenario, time) + getSecondary(scenario, secondaryTime);
+		return getPrimary(scenario, minDelay(scenario.getDelay(), time)) + getSecondary(scenario, secondaryTime);
 	}
 
-	private double getPrimary(Scenario scenario, int time) {
-		return 0.5 * scenario.primaryAcceleration() * minDelay(scenario.getDelay(), time) * minDelay(scenario.getDelay(), time);
+	private double getPrimary(Scenario scenario, double primaryTime) {
+		return 0.5 * scenario.primaryAcceleration() * primaryTime * primaryTime;
 	}
 
 	private double minDelay(double delay, int time) {
