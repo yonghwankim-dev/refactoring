48ef24b5286e16b0e7541f287263bff17111adf7
yonghwankim-dev <dragonbead95@naver.com>
2024-08-18 13:28:40 +0900

    refactor: filter 추가

--- src/main/java/com/nemo/refactoring/ch08/class08/OfficeCsvReader.java
+++ src/main/java/com/nemo/refactoring/ch08/class08/OfficeCsvReader.java
@@ -9,15 +9,12 @@ import org.apache.logging.log4j.util.Strings;
 public class OfficeCsvReader {
 	public List<Office> readOffices(String input) {
 		String[] lines = input.split("\n");
-		boolean firstLine = true;
 		List<Office> result = new ArrayList<>();
 		String[] loopItems = Arrays.stream(lines)
 			.skip(1)
+			.filter(line->!line.trim().equals(Strings.EMPTY))
 			.toArray(String[]::new);
 		for (String line : loopItems) {
-			if (line.trim().equals(Strings.EMPTY)){
-				continue;
-			}
 			String[] record = line.split(",");
 			if (record[1].trim().equals("India")){
 				result.add(new Office(record[0].trim(), record[1].trim(), record[2].trim()));
