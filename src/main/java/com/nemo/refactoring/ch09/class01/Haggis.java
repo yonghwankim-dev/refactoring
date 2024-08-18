34cd57078cd7e2e91d1ea37596f243b2f39494a6
yonghwankim-dev <dragonbead95@naver.com>
2024-08-18 13:52:46 +0900

    refactor: 변수명 변경

--- src/main/java/com/nemo/refactoring/ch09/class01/Haggis.java
+++ src/main/java/com/nemo/refactoring/ch09/class01/Haggis.java
@@ -4,13 +4,13 @@ public class Haggis {
 
 	public double distanceTravelled(Scenario scenario, int time){
 		double result = 0;
-		double acc = scenario.getPrimaryForce() / scenario.getMass(); // 가속도 = 힘 / 질량
+		final double primaryAcceleration = scenario.getPrimaryForce() / scenario.getMass(); // 가속도 = 힘 / 질량
 		double primaryTime = Math.min(time, scenario.getDelay());
-		result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
+		result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 전파된 거리
 		double secondaryTime = time - scenario.getDelay();
 		if(secondaryTime > 0) {
-			double primaryVelocity = acc * scenario.getDelay();
-			acc = (scenario.getPrimaryForce() + scenario.getSecondaryForce()) / scenario.getMass();
+			double primaryVelocity = primaryAcceleration * scenario.getDelay();
+			double acc = (scenario.getPrimaryForce() + scenario.getSecondaryForce()) / scenario.getMass();
 			result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
 		}
 		return result;
