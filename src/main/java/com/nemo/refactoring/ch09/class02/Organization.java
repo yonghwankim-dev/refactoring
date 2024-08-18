d4fbab4f674a96a3ef9e015304c0e075a3e53551
yonghwankim-dev <dragonbead95@naver.com>
2024-08-18 14:20:11 +0900

    refacotr: Organization 클래스의 name 필드를 title로 변경

--- src/main/java/com/nemo/refactoring/ch09/class02/Organization.java
+++ src/main/java/com/nemo/refactoring/ch09/class02/Organization.java
@@ -1,16 +1,16 @@
 package com.nemo.refactoring.ch09.class02;
 
 public class Organization {
-	private final String name;
+	private final String title;
 	private final String country;
 
 	public Organization(String name, String country) {
-		this.name = name;
+		this.title = name;
 		this.country = country;
 	}
 
 	public String getName() {
-		return name;
+		return title;
 	}
 
 	public String getCountry() {
@@ -19,6 +19,6 @@ public class Organization {
 
 	@Override
 	public String toString() {
-		return name + ", " + country;
+		return title + ", " + country;
 	}
 }
