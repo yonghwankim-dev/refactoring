1fd0ded77e911b215d9e28c67b52f616b9a67ead
yonghwankim-dev <dragonbead95@naver.com>
2024-08-24 13:22:37 +0900

    refactor: bottomBond 메서드 제거

--- src/main/java/com/nemo/refactoring/ch11/class02/step02/ChargeCalculator.java
+++ src/main/java/com/nemo/refactoring/ch11/class02/step02/ChargeCalculator.java
@@ -5,14 +5,10 @@ public class ChargeCalculator {
 		if (usage < 0) {
 			return usd(0);
 		}
-		int amount = (int)(bottomBond(usage) * 0.03)
+		int amount = (int)(withinBand(usage, 0, 100) * 0.03)
 			+ (int)(withinBand(usage, 100, 200) * 0.05)
 			+ (int)(topBand(usage) * 0.07);
 		return usd(amount);
-	}
-
-	private int bottomBond(int usage) {
-		return Math.min(usage, 100);
 	}
 
 	private int withinBand(int usage, int bottom, int top) {
