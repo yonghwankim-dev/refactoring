05896dd81d0e2fd57d10670f184861ce087db083
yonghwankim-dev <dragonbead95@naver.com>
2024-08-24 13:52:42 +0900

    refactor: 메서드 추출

--- src/main/java/com/nemo/refactoring/ch11/class03/step01/Delivery.java
+++ src/main/java/com/nemo/refactoring/ch11/class03/step01/Delivery.java
@@ -6,25 +6,33 @@ import java.util.List;
 public class Delivery {
 	public LocalDate deliveryDate(Order order, boolean isRush){
 		if(isRush){
-			int deliveryTime = 0;
-			if (List.of("MA", "CT").contains(order.getDeliveryState())){
-				deliveryTime += 1;
-			}else if (List.of("NY", "NH").contains(order.getDeliveryState())){
-				deliveryTime += 2;
-			}else{
-				deliveryTime += 3;
-			}
-			return order.getPlacedOn().plusDays(1 + deliveryTime);
+			return rushDeliveryDate(order);
 		}else{
-			int deliveryTime = 0;
-			if (List.of("MA", "CT", "NY").contains(order.getDeliveryState())){
-				deliveryTime += 2;
-			}else if (List.of("ME", "NH").contains(order.getDeliveryState())){
-				deliveryTime += 3;
-			}else{
-				deliveryTime += 4;
-			}
-			return order.getPlacedOn().plusDays(2 + deliveryTime);
+			return regularDeliveryDate(order);
 		}
+	}
+
+	private LocalDate rushDeliveryDate(Order order) {
+		int deliveryTime = 0;
+		if (List.of("MA", "CT").contains(order.getDeliveryState())){
+			deliveryTime += 1;
+		}else if (List.of("NY", "NH").contains(order.getDeliveryState())){
+			deliveryTime += 2;
+		}else{
+			deliveryTime += 3;
+		}
+		return order.getPlacedOn().plusDays(1 + deliveryTime);
+	}
+
+	private LocalDate regularDeliveryDate(Order order) {
+		int deliveryTime = 0;
+		if (List.of("MA", "CT", "NY").contains(order.getDeliveryState())){
+			deliveryTime += 2;
+		}else if (List.of("ME", "NH").contains(order.getDeliveryState())){
+			deliveryTime += 3;
+		}else{
+			deliveryTime += 4;
+		}
+		return order.getPlacedOn().plusDays(2 + deliveryTime);
 	}
 }
