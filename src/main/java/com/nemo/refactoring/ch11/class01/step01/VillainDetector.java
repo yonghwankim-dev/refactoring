e7d624c2a912d37b8fa32e5d2f5d311d01d7ff6a
yonghwankim-dev <dragonbead95@naver.com>
2024-08-24 12:30:42 +0900

    refactor: 메서드를 질의함수에 맞게 이름 변경

---
+++ src/main/java/com/nemo/refactoring/ch11/class01/step01/VillainDetector.java
@@ -0,0 +1,25 @@
+package com.nemo.refactoring.ch11.class01.step01;
+
+import java.util.List;
+
+import org.apache.logging.log4j.util.Strings;
+
+public class VillainDetector {
+	public String findMiscreant(List<String> people) {
+		for (String p : people){
+			if (p.equals("jocker")){
+				sendAlert();
+				return "jocker";
+			}
+			if (p.equals("saruman")){
+				sendAlert();
+				return "saruman";
+			}
+		}
+		return Strings.EMPTY;
+	}
+
+	private void sendAlert() {
+		System.out.println("alert villain!!");
+	}
+}
---
+++ src/test/java/com/nemo/refactoring/ch11/class01/step01/VillainDetectorTest.java
@@ -0,0 +1,23 @@
+package com.nemo.refactoring.ch11.class01.step01;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class VillainDetectorTest {
+
+	@DisplayName("jocker는 빌런 탐색기에 발견되어 알람을 울린다")
+	@Test
+	void checkForMiscreants(){
+	    // given
+		VillainDetector detector = new VillainDetector();
+		List<String> people = List.of("jocker", "poisonivy", "mario");
+		// when
+		String villain = detector.findMiscreant(people);
+		// then
+		Assertions.assertThat(villain).isEqualTo("jocker");
+	}
+
+}
