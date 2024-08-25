d346e5179f2646d24b13893c97ee7eb657684af4
yonghwankim-dev <dragonbead95@naver.com>
2024-08-25 16:32:10 +0900

    refactor: 세터 제거

---
+++ src/main/java/com/nemo/refactoring/ch11/class07/Person.java
@@ -0,0 +1,11 @@
+package com.nemo.refactoring.ch11.class07;
+
+public class Person {
+	private final int id;
+	private final String name;
+
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+}
